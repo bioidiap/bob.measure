e885c41bb8d1d21dfc5fc298814544e00290efa1
Andre Anjos <andre.dos.anjos@gmail.com>
2014-03-17 09:15:20 +0100

    Fix python3 compatibility issues with doc fix

--- xbob/measure/__init__.py
+++ xbob/measure/__init__.py
@@ -127,3 +127,6 @@ def cmc(cmc_scores):
     cumulative_match_characteristic[i] = count / probe_count
 
   return cumulative_match_characteristic
+
+# gets sphinx autodoc done right - don't remove it
+__all__ = [_ for _ in dir() if not _.startswith('_')]
