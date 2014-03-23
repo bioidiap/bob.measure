28fa6c57438a40cccdeb0ae074180fee4547fee0
Andre Anjos <andre.dos.anjos@gmail.com>
2014-03-23 07:51:16 +0100

    Add function to print comprehensive dependence list

--- xbob/measure/__init__.py
+++ xbob/measure/__init__.py
@@ -130,5 +130,24 @@ def cmc(cmc_scores):
 
   return cumulative_match_characteristic
 
+def get_config():
+  """Returns a string containing the configuration information.
+  """
+
+  import pkg_resources
+  from .version import externals
+
+  packages = pkg_resources.require(__name__)
+  this = packages[0]
+  deps = packages[1:]
+
+  retval =  "%s: %s (%s)\n" % (this.key, this.version, this.location)
+  retval += "  - c/c++ dependencies:\n"
+  for k in sorted(externals): retval += "    - %s: %s\n" % (k, externals[k])
+  retval += "  - python dependencies:\n"
+  for d in deps: retval += "    - %s: %s (%s)\n" % (d.key, d.version, d.location)
+
+  return retval.strip()
+
 # gets sphinx autodoc done right - don't remove it
 __all__ = [_ for _ in dir() if not _.startswith('_')]
