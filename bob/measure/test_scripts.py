6685d8eb8dd64e76ee790cd912ad9a065533c3e3
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2017-11-13 18:34:21 +0100

    Provide a test case for negative thresholds

--- bob/measure/test_scripts.py
+++ bob/measure/test_scripts.py
@@ -86,6 +86,15 @@ def test_apply_threshold():
   from .script.apply_threshold import main
   nose.tools.eq_(main(cmdline), 0)
 
+  cmdline = [
+      '--',
+      '-0.5',
+      TEST_SCORES,
+  ]
+
+  from .script.apply_threshold import main
+  nose.tools.eq_(main(cmdline), 0)
+
 
 def test_compute_perf_5col():
 
