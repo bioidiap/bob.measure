91b44bd948cbd5bd33f557f2067c9e44994e450e
Theophile GENTILHOMME <tgentilhomme@jurasix08.idiap.ch>
2018-03-20 16:13:50 +0100

    Fix doc and add guide description

--- bob/measure/utils.py
+++ bob/measure/utils.py
@@ -9,24 +9,20 @@ def confidence_for_indicator_variable(x, n, alpha=0.05):
     The Clopper-Pearson interval method is used for estimating the confidence
     intervals.
 
-    More info on confidence intervals
-    ---------------------------------
-    https://en.wikipedia.org/wiki/Confidence_interval
-    https://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval#Clopper-Pearson_interval
-
     Parameters
     ----------
     x : int
         The number of successes.
     n : int
         The number of trials.
-    alpha : float, optional
+        alpha : :obj:`float`, optional
         The 1-confidence value that you want. For example, alpha should be 0.05
         to obtain 95% confidence intervals.
 
     Returns
     -------
-    (float, float) Returns a tuple of (lower_bound, upper_bound) which
+    (:obj:`float`, :obj:`float`)
+        a tuple of (lower_bound, upper_bound) which
         shows the limit of your success rate: lower_bound < x/n < upper_bound
     '''
     lower_bound = scipy.stats.beta.ppf(alpha / 2.0, x, n - x + 1)
