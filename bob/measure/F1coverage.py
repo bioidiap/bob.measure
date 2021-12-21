770df795166b979031ed9abd78087892e2949f92
Antonio MORAIS <amorais@lentix14.idiap.ch>
2021-12-21 15:01:56 +0100

    modifications on F1 coverage and crossfolding

--- bob/measure/F1coverage.py
+++ bob/measure/F1coverage.py
@@ -6,18 +6,21 @@ import scipy.stats
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 
-def F1_percentage(nb_samples, nb_trials, coverage, lambda_, truepositivesprob, truenegativesprob):
+def F1_percentage(nb_samples, nb_positives, coverage, lambda_, truepositivesprob, truenegativesprob, ratio):
     percentage_covered = np.empty([truepositivesprob.size, truenegativesprob.size])
+    precisions = np.empty([truepositivesprob.size, truenegativesprob.size])
+    recalls = np.empty([truepositivesprob.size, truenegativesprob.size])
     for index, tpprob in enumerate(truepositivesprob):
-        tpbin = np.random.binomial(nb_trials, tpprob, nb_samples)
-        fnbin = nb_trials - tpbin
-        tp = int(round(tpprob*nb_trials))
-        fn = nb_trials - tp
+        tpbin = np.random.binomial(nb_positives, tpprob, nb_samples)
+        fnbin = nb_positives - tpbin
+        tp = int(round(tpprob*nb_positives))
+        fn = nb_positives - tp
         for index1, tnprob in enumerate(truenegativesprob):
-            tnbin = np.random.binomial(nb_trials, tnprob, nb_samples)
-            fpbin = nb_trials - tnbin
-            tn = int(round(tnprob*nb_trials))
-            fp = nb_trials - tn
+            nb_negatives = int(round(nb_positives * ratio))
+            tnbin = np.random.binomial(nb_negatives, tnprob, nb_samples)
+            fpbin = nb_negatives - tnbin
+            tn = int(round(tnprob*nb_negatives))
+            fp = nb_negatives - tn
             F1scores = np.empty(nb_samples)
             for i in range(nb_samples) :
                 if (tpbin[i]+fpbin[i]) == 0 :
@@ -36,15 +39,18 @@ def F1_percentage(nb_samples, nb_trials, coverage, lambda_, truepositivesprob, t
             _,_, lowerCi, upperCi = f1_score(tp, fp, fn, lambda_, coverage, nb_samples)
             sorted_f1scores = np.sort(F1scores)
             percentage_covered[index][index1] = np.count_nonzero((upperCi > sorted_f1scores) & (sorted_f1scores > lowerCi)) / nb_samples
-    return percentage_covered
+            precisions[index][index1] = 0 if (tp + fp) == 0 else tp/(tp + fp)
+            recalls[index][index1] = 0 if (tp + fn) == 0 else tp/(tp + fn)
+    return percentage_covered, recalls, precisions
 
 truepositivesprob = np.arange(0.01, 1, 0.01) #x
 truenegativesprob = np.arange(0.01, 1, 0.01) #y
 nb_samples = 100
-nb_trials = 100
+nb_positives = 100
 coverage = 0.95
 lambda_ = 0.5
-percentage_covered = F1_percentage(nb_samples, nb_trials, coverage, lambda_, truepositivesprob, truenegativesprob) #z
+ratio = 1
+percentage_covered, recalls, precisions = F1_percentage(nb_samples, nb_positives, coverage, lambda_, truepositivesprob, truenegativesprob, ratio) #z
 
 # Plot the surface.
 from matplotlib import cm
@@ -54,22 +60,23 @@ fig = plt.figure(figsize=plt.figaspect(0.5))
 ax1 = fig.add_subplot(1, 2, 1, projection='3d')
 ax2 = fig.add_subplot(1, 2, 2)
 
-X, Y = np.meshgrid(truepositivesprob, truenegativesprob)
-surf = ax1.plot_surface(X, Y, percentage_covered, cmap=cm.coolwarm, linewidth=0)
-cont = ax2.contourf(X, Y, percentage_covered, cmap=cm.coolwarm)
 
-ax1.set_xlabel("TPR")
-ax1.set_ylabel("TNR")
+# X, Y = np.meshgrid(truepositivesprob, truenegativesprob)
+surf = ax1.plot_surface(precisions, recalls, percentage_covered, cmap=cm.coolwarm, linewidth=0)
+cont = ax2.contourf(precisions, recalls, percentage_covered, cmap=cm.coolwarm)
+
+ax1.set_xlabel("precision")
+ax1.set_ylabel("recall")
 ax1.set_zlabel("Coverage")
-ax2.set_xlabel("TPR")
-ax2.set_ylabel("TNR")
+ax2.set_xlabel("precision")
+ax2.set_ylabel("recall")
 ax2.set_title("Coverage")
 
 # Customize the z axis.
 ax1.set_zlim(0, 1)
 ax1.zaxis.set_major_locator(LinearLocator(10))
 # A StrMethodFormatter is used automatically
-ax1.zaxis.set_major_formatter('{x:.02f}')
+# ax1.zaxis.set_major_formatter('{x:.02f}')
 
 # Add a color bar which maps values to colors.
 fig.colorbar(cont, shrink=0.5, aspect=5)
--- bob/measure/crossfolding.py
+++ bob/measure/crossfolding.py
@@ -1,58 +1,43 @@
 from credible_region import beta
-from credible_region import beta_posterior
 import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-def cross_folding(TP1, FP1, TP2, FP2, nb_samples):
-    # first cross folding
-    numTrials1 = TP1 + FP1
-    pool1 = np.append(np.zeros(FP1), np.ones(TP1))
-    np.random.shuffle(pool1)
-    set1_1, set1_2 = np.array_split(pool1, 2)
-    tp1_1 = np.count_nonzero(set1_1)
-    fp1_1 = set1_1.size - tp1_1
-    tp1_2 = np.count_nonzero(set1_2)
-    fp1_2 = set1_2.size - tp1_2
-    beta_post1_1 = beta_posterior(tp1_1, fp1_1, 0.5, nb_samples)
-    beta_post1_2 = beta_posterior(tp1_2, fp1_2, 0.5, nb_samples)
-    #second cross folding
-    numTrials2 = TP2 + FP2
-    pool2 = np.append(np.zeros(FP2), np.ones(TP2))
-    np.random.shuffle(pool2)
-    set2_1, set2_2 = np.array_split(pool2, 2)
-    tp2_1 = np.count_nonzero(set2_1)
-    fp2_1 = set2_1.size - tp2_1
-    tp2_2 = np.count_nonzero(set2_2)
-    fp2_2 = set2_2.size - tp2_2
-    beta_post2_1 = beta_posterior(tp2_1, fp2_1, 0.5, nb_samples)
-    beta_post2_2 = beta_posterior(tp2_2, fp2_2, 0.5, nb_samples)
-    return np.count_nonzero(beta_post1_1 > beta_post1_2) / nb_samples, np.count_nonzero(beta_post2_1 > beta_post2_2) / nb_samples
+def folding_monte_carlo(TP1, FP1, TP2, FP2, nb_samples):
+    lower_scores = np.empty(nb_samples)
+    upper_scores = np.empty(nb_samples)
+    numTrials1 = (TP1 + FP1)
+    tp1prob = TP1 / numTrials1
+    tp1bin = np.random.binomial(numTrials1, tp1prob, nb_samples)
+    fp1bin = numTrials1 - tp1bin
+    numTrials2 = (TP2 + FP2)
+    tp2prob = TP2 / numTrials2
+    tp2bin = np.random.binomial(numTrials2, tp2prob, nb_samples)
+    fp2bin = numTrials2 - tp2bin
+    for i in range(nb_samples):
+        _,_, lowerCiset1, upperCiset1 = beta(tp1bin[i], fp1bin[i], 0.5, 0.95)
+        _,_, lowerCiset2, upperCiset2 = beta(tp2bin[i], fp2bin[i], 0.5, 0.95)
+        lowerCi_mean = (lowerCiset1 + lowerCiset2) / 2
+        lower_scores[i] = lowerCi_mean
+        upperCi_mean = (upperCiset1 + upperCiset2) / 2
+        upper_scores[i] = upperCi_mean
+    return np.mean(lower_scores), np.mean(upper_scores)
 
-first_cross_comp, second_cross_comp = cross_folding(50, 50, 52, 48, 100)
-print(first_cross_comp)
-print(second_cross_comp)
+def cross_folding(TP, FP, nb_samples, nb_cross_folding):
+    lower_cross_folding_scores = np.empty(nb_cross_folding)
+    upper_cross_folding_scores = np.empty(nb_cross_folding)
+    for i in range(nb_cross_folding):
+        numTrials = TP + FP
+        pool = np.append(np.zeros(FP), np.ones(TP))
+        np.random.shuffle(pool)
+        set1, set2 = np.array_split(pool, 2)
+        tp1 = np.count_nonzero(set1)
+        fp1 = set1.size - tp1
+        tp2 = np.count_nonzero(set2)
+        fp2 = set2.size - tp2
+        lower_cross_folding_scores[i], upper_cross_folding_scores[i] = folding_monte_carlo(tp1, fp1, tp2, fp2, nb_samples)
+    return np.mean(lower_cross_folding_scores), np.mean(upper_cross_folding_scores)
 
-# def cross_folding(TP1, FP1, TP2, FP2, nb_samples):
-#     lower_scores = np.empty(nb_samples)
-#     upper_scores = np.empty(nb_samples)
-#     numTrials1 = (TP1 + FP1)
-#     tp1prob = TP1 / numTrials1
-#     tp1bin = np.random.binomial(numTrials1, tp1prob, nb_samples)
-#     fp1bin = numTrials1 - tp1bin
-#     numTrials2 = (TP2 + FP2)
-#     tp2prob = TP2 / numTrials2
-#     tp2bin = np.random.binomial(numTrials2, tp2prob, numSamples)
-#     fp2bin = numTrials2 - tp2bin
-#     for i in range(nb_samples):
-#         _,_, lowerCiset1, upperCiset1 = beta(tp1bin[i], fp1bin[i], 0.5, 0.95)
-#         _,_, lowerCiset2, upperCiset2 = beta(tp2bin[i], fp2bin[i], 0.5, 0.95)
-#         lowerCi_mean = (lowerCiset1 + lowerCiset2) / 2
-#         lower_scores[i] = lowerCi_mean
-#         upperCi_mean = (upperCiset1 + upperCiset2) / 2
-#         upper_scores[i] = upperCi_mean
-#     return lower_scores, upper_scores
-
-# low, upp = cross_folding(50, 50, 52, 48, 100)
-# print(low)
-# print(upp)
+low, upp = cross_folding(100, 100, 100, 20)
+print(low)
+print(upp)
